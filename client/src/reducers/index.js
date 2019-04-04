8719dc223f4facb4ddb1ebe0dac79a2adfd43555
Dipanshu <dipanshuchaubey@gmail.com>
2019-04-04 23:29:51 +0530

    Redirect to github issue solved

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import errorReducers from "./errorReducer";
 import authReducer from "./authReducer";
 import profileReducer from "./profileReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducers,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 });
