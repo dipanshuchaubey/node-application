06198bb6bcc2c2bf90fe5c40a92e20246029759b
Dipanshu <dipanshuchaubey@gmail.com>
2019-04-07 23:28:29 +0530

    Posts Comments updated

--- client/src/components/posts/Comments.js
+++ client/src/components/posts/Comments.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class Comments extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment,
+      show,
+      postId
+    };
+  }
   render() {
     return <div />;
   }
 }
 
-export default Comments;
+Comment.propTypes = {
+  comments: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  comments: state.comments
+});
+
+export default connect(mapStateToProps)(Comments);
